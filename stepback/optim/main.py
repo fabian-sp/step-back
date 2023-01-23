ca82ce572d0d867133bb8e8869ead1c74d4e8afd
Fabian Schaipp <fabian.schaipp@hotmail.de>
2023-01-23 09:40:41 +0100

    add lb as option

--- stepback/optim/main.py
+++ stepback/optim/main.py
@@ -68,6 +68,7 @@ def get_optimizer(opt_config: dict) -> (torch.optim.Optimizer, dict):
         hyperp = {'lr': opt_config.get('lr', 1e-3),
                   'weight_decay': opt_config.get('weight_decay', 0),
                   'beta': opt_config.get('beta', 0.9),
+                  'lb': opt_config.get('lb', 0.),
                   'bias_correction': opt_config.get('bias_correction', True)
                   }
         
