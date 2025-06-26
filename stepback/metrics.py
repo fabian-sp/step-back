ca6a98680d84879e86dac47937c64ec95c4dc3f1
fabian-sp <fabian.schaipp@hotmail.de>
2025-06-26 18:22:07 +0200

    initial llama model and loss

--- stepback/metrics.py
+++ stepback/metrics.py
@@ -5,13 +5,13 @@ NOTE:
     
     * We generally use reduction='mean' for all loss functions.
     * When using the squared loss (MSELoss), we flatten output and target. 
-      Due to reduction='mean', this computes the avergae error, per tensor entry and over the batch.
+      Due to reduction='mean', this computes the average error, per tensor entry and over the batch.
       E.g. with batch size b and output dimension m, the loss is compute as 1/(b*m) sum (output-target)^2.
       Actually, this is also what Pytorch does internally.
        
 """
-import torch 
-
+import torch
+from torch.nn import functional as F
 
 class Loss:
     def __init__(self, name : str, backwards: bool=False):
@@ -37,6 +37,13 @@ class Loss:
         if self.name == 'cross_entropy':
             self.criterion = torch.nn.CrossEntropyLoss()
         
+        # for token sequences
+        # target shape: (b, seq_len) --> do not flatten
+        # TODO: extract from name an optional label smoothing
+        elif self.name == 'sequence_cross_entropy':
+            self.criterion = SequenceCrossEntropyLoss()
+            self._flatten_target = False
+
         elif self.name == 'logistic':
             self.criterion = torch.nn.SoftMarginLoss()
             self._flatten_out = True
@@ -70,7 +77,37 @@ class Loss:
         
         return loss
 
+class SequenceCrossEntropyLoss(torch.nn.modules.loss._Loss):
+    def __init__(self, reduction: str='mean', label_smoothing: float=0.0):
+        super().__init__(reduction=reduction)
+        self.label_smoothing = label_smoothing
+        return
+    
+    def forward(self, input: torch.Tensor, target: torch.Tensor) -> torch.Tensor:
+        """Compute cross-entropy loss of a sequential output (of a language model).
+        b: batch size
+        m: sequence length
+        C: vocabulary size
 
+        Parameters
+        ----------
+        input : torch.Tensor
+            Model logits. Shape (b,m,C)
+        target : torch.Tensor
+            Shape (b,m)
+
+        Returns
+        -------
+        torch.Tensor
+            Loss value.
+        """
+        L = F.cross_entropy(input.view(-1, input.size(-1)),
+                            target.view(-1),
+                            ignore_index=-1,
+                            reduction=self.reduction,
+                            label_smoothing=self.label_smoothing
+        )
+        return L
 
 ##
 # Accuracy functions
